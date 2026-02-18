e281068d9f63e4a55d0be061b823572f031b00a0
hamadehzaher0@gmail.com <zuz@zuz-mac.local>
2026-02-18 11:44:22 +0100

    drawing circle

--- src/main.zig
+++ src/main.zig
@@ -2,17 +2,66 @@ const std = @import("std");
 const arealtimestrategygame = @import("arealtimestrategygame");
 const rl = @import("raylib");
 
+const Shape = union(enum) {
+    circle: struct {
+        center: rl.Vector3,
+        radius: f32,
+    },
+    triangle: struct {
+        points: [3]rl.Vector3,
+    },
+
+    fn draw(self: *const Shape) void {
+        switch (self.*) {
+            .circle => |*circle| {
+                rl.drawCircleV(
+                    .{
+                        .x = circle.center.x,
+                        .y = circle.center.y,
+                    },
+                    circle.radius,
+                    .red,
+                );
+            },
+            .triangle => |*triangle| {
+                rl.drawTriangle(
+                    .{ .x = triangle.points[0].x, .y = triangle.points[0].y },
+                    .{ .x = triangle.points[1].x, .y = triangle.points[1].y },
+                    .{ .x = triangle.points[2].x, .y = triangle.points[2].y },
+                    .blue,
+                );
+            },
+        }
+    }
+};
+
+const Entity = struct {
+    id: usize,
+    shape: Shape,
+
+    fn draw(self: *const Entity) void {
+        self.shape.draw();
+    }
+};
+
 pub fn main() anyerror!void {
     // Initialization
     //--------------------------------------------------------------------------------------
     const screenWidth = 800;
     const screenHeight = 450;
 
-    rl.initWindow(screenWidth, screenHeight, "raylib-zig [core] example - basic window");
+    rl.initWindow(screenWidth, screenHeight, "aRealTimeStrategyGame");
     defer rl.closeWindow(); // Close window and OpenGL context
 
     rl.setTargetFPS(60); // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
+
+    const entity1 = Entity{
+        .id = 1,
+        .shape = .{
+            .circle = .{ .center = .{ .x = 0, .y = 0, .z = 0 }, .radius = 10 },
+        },
+    };
 
     // Main game loop
     while (!rl.windowShouldClose()) { // Detect window close button or ESC key
@@ -27,8 +76,8 @@ pub fn main() anyerror!void {
         defer rl.endDrawing();
 
         rl.clearBackground(.white);
-
-        rl.drawText("Congrats! You created your first window!", 190, 200, 20, .light_gray);
+        entity1.draw();
+        // rl.drawText("Congrats! You created your first window!", 190, 200, 20, .light_gray);
         //----------------------------------------------------------------------------------
     }
 }
